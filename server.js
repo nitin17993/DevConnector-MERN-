170414789acfc70a74f0b70db90873815e6b2e5f
Nitin2.Dixit <nitin17993@gmail.com>
2020-08-28 14:01:39 +0530

    prod-changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { body } = require("express-validator");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 // Connect Database
 connectDB();
@@ -11,13 +12,22 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
